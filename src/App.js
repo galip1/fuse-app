2c8c37a3390d8f5935ce195712d4703cb22bc907
galip <galipts1@gmail.com>
2023-09-19 19:17:49 +0300

    v3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CustomRoutes from "./router/custom-routes";
+import LoadingPage from "./pages/loading-page";
 
 const App = () => {
-  return (
-    <>
-      <CustomRoutes />
-    </>
-  );
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setLoading(false);
+  }, [loading]);
+
+  return <div>{loading ? <LoadingPage /> : <CustomRoutes />}</div>;
 };
 
 export default App;
